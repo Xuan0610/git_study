2bc7ea2c42043ab73549534451aedf9e914d7999
Grace <yxc081224@gmail.com>
2025-01-14 18:38:06 +0800

    Add: 註解解答

--- all.js
+++ all.js
@@ -1,5 +1,7 @@
-let a = true || false;
-let b = 1 && 0;
-let c = (4 > 3) && (2 >= 2);
-let d = !(4 > 3);
-let e = true && (1 > 2);
+let a = true || false; //Ans: true
+let b = 1 && 0; //Ans: 0
+let c = (4 > 3) && (2 >= 2); //Ans: true
+let d = !(4 > 3); //Ans:false 
+let e = true && (1 > 2); //Ans: true
+
+console.log(a, b, c, d, e);
