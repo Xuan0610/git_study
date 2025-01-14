ccb9ce8785a9530ac4ba73311f0c8151f902d8c2
Grace <yxc081224@gmail.com>
2025-01-14 20:55:30 +0800

    Change: 修正解答

--- all.js
+++ all.js
@@ -2,6 +2,6 @@ let a = true || false; //Ans: true
 let b = 1 && 0; //Ans: 0
 let c = (4 > 3) && (2 >= 2); //Ans: true
 let d = !(4 > 3); //Ans:false 
-let e = true && (1 > 2); //Ans: true
+let e = true && (1 > 2); //Ans: false
 
 console.log(a, b, c, d, e);
