1739e85577f51858d8fa8293805240e04df20a36
Grace <yxc081224@gmail.com>
2025-01-15 11:48:11 +0800

    Add: Day 8 homework

--- all.js
+++ all.js
@@ -5,3 +5,22 @@ let d = !(4 > 3); //Ans:false
 let e = true && (1 > 2); //Ans: false
 
 console.log(a, b, c, d, e);
+
+
+// Day 08 - 運算子大雜燴
+// 1. 請問變數 c 的值
+let a = 1;
+let b = "1";
+let c = (a===b) && (a!==b);
+
+
+// 2. 請問變數 g 的值
+let e = '123'; 
+let f = 456;
+let g = (e+f) === '123456'; 
+
+// 3. 請問變數 j, k 的值 
+let h = true;
+let i = false;
+let j = 1 == h;
+let k = h || i;
