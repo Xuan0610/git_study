d6b01c9c7d911152b20a1fa763a39f121af02d11
Grace <yxc081224@gmail.com>
2025-01-15 11:54:06 +0800

    Add: 我的答案

--- all.js
+++ all.js
@@ -12,15 +12,17 @@ console.log(a, b, c, d, e);
 let a = 1;
 let b = "1";
 let c = (a===b) && (a!==b);
-
+// Ans:false
 
 // 2. 請問變數 g 的值
 let e = '123'; 
 let f = 456;
 let g = (e+f) === '123456'; 
+// Ans:true
 
 // 3. 請問變數 j, k 的值 
 let h = true;
 let i = false;
 let j = 1 == h;
 let k = h || i;
+//Ans:j true ; k true
