d0107efeffcd144eca1cbf071c628c39c81a3176
Grace <yxc081224@gmail.com>
2025-01-16 10:18:34 +0800

    Add: Day 9 homework

--- all.js
+++ all.js
@@ -19,3 +19,23 @@ let k = h || i;
 //Ans:j true ; k true
 
 console.log(c, g, j, k);
+
+
+// Day 09 - if、else if、else 練習一
+//題目一 如果下雨我就坐捷運上班，沒有我就騎車上班
+let weather = '雨天';
+
+/* 請試著寫出判斷式 */
+
+//題目二 如果 BMI > 24 就超重，18 ~ 24 體重正常，小於 18 過輕。（提示：補上問號部分)
+let myBMI = 19;
+
+/* 請試著寫出判斷式 */
+
+if( ??? ) {
+  console.log('過重');
+} else if( ??? ) {
+  console.log('體重正常');
+} else {
+  console.log('過輕');
+}
