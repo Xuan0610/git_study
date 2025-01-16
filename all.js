b22416bf32c2f0f32a5b6a9098561dba704aa005
Grace <yxc081224@gmail.com>
2025-01-16 10:13:34 +0800

    Delete: Day 7 homework

--- all.js
+++ all.js
@@ -1,12 +1,3 @@
-let a = true || false; //Ans: true
-let b = 1 && 0; //Ans: 0
-let c = (4 > 3) && (2 >= 2); //Ans: true
-let d = !(4 > 3); //Ans:false 
-let e = true && (1 > 2); //Ans: false
-
-console.log(a, b, c, d, e);
-
-
 // Day 08 - 運算子大雜燴
 // 1. 請問變數 c 的值
 let a = 1;
