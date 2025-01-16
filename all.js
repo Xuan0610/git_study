e3ea16c43efb3d50245268c5bdded8a2c22ba9ae
Grace <yxc081224@gmail.com>
2025-01-16 10:22:33 +0800

    Add: Answer for no.1

--- all.js
+++ all.js
@@ -26,6 +26,11 @@ console.log(c, g, j, k);
 let weather = '雨天';
 
 /* 請試著寫出判斷式 */
+if(weather==="雨天"){
+    console.log("我坐捷運上班");
+}else{
+    console.log("我騎車上班");
+}
 
 //題目二 如果 BMI > 24 就超重，18 ~ 24 體重正常，小於 18 過輕。（提示：補上問號部分)
 let myBMI = 19;
